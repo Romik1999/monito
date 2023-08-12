ee2f53725bf4b3f325cd99b1db6571cf7296a9f1
Roman <usankin.roma@gmail.com>
2023-08-12 13:37:57 +0400

    - write icons - add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import axios from 'axios'
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import CheckIcon from '@mui/icons-material/Check';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import EastRoundedIcon from '@mui/icons-material/EastRounded';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Hello word!</h1>
+                <PlayCircleOutlinedIcon/>
+                <KeyboardArrowRightOutlinedIcon/>
+                <KeyboardArrowDownRoundedIcon/>
+                <FacebookOutlinedIcon/>
+                <TwitterIcon/>
+                <InstagramIcon/>
+                <YouTubeIcon/>
+                <CheckIcon/>
+                <TextsmsOutlinedIcon/>
+                <EastRoundedIcon/>
             </header>
         </div>
     );
