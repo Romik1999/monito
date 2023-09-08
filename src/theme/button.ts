38b72969ba396b96cacd05145b25e8b5b8fe833b
Roman <usankin.roma@gmail.com>
2023-09-08 23:08:30 +0400

    - adding dynamic palette import from themes for button

--- src/theme/button.ts
+++ src/theme/button.ts
@@ -1,39 +1,42 @@
-import {Components, Theme} from "@mui/material";
-import palette from "./palette";
+import {Components} from "@mui/material";
+import THEME from "./index";
 
 const button: Components[`MuiButton`] = {
     styleOverrides: {
-        root: ({theme}) => {
-            const {palette} = theme as Theme
-            return {
-                padding: `14px 28px 10px`,
-                borderRadius: '57px',
-                boxSizing: "border-box"
-            }
+        root: {
+            padding: `14px 28px 10px`,
+            borderRadius: '57px',
+            boxSizing: "border-box"
         }
     },
     variants: [
         {
             props: {color: 'primary'},
-            style: {
-                backgroundColor: palette.primary.main,
-                color: palette.white.main,
-                border: `1px solid ${palette.primary.main}`,
-                '&:hover': {
-                    backgroundColor: 'transparent',
-                    color: palette.primary.main
+            style: (theme) => {
+                const {palette} = THEME
+                return {
+                    backgroundColor: palette.primary.main,
+                    color: palette.white.main,
+                    border: `1px solid ${palette.primary.main}`,
+                    '&:hover': {
+                        backgroundColor: 'transparent',
+                        color: palette.primary.main
+                    }
                 }
             }
         },
         {
             props: {color: 'secondary'},
-            style: {
-                backgroundColor: 'transparent',
-                color: palette.primary.main,
-                border: `1px solid ${palette.primary.main}`,
-                '&:hover': {
-                    backgroundColor: palette.primary.main,
-                    color: palette.white.main
+            style: (theme) => {
+                const {palette} = THEME
+                return {
+                    backgroundColor: 'transparent',
+                    color: palette.primary.main,
+                    border: `1px solid ${palette.primary.main}`,
+                    '&:hover': {
+                        backgroundColor: palette.primary.main,
+                        color: palette.white.main
+                    }
                 }
             }
         }
