12900f5337d3861e4bb18fb932a86a32cafc8b4d
Roman <usankin.roma@gmail.com>
2023-09-23 03:07:04 +0400

    - add react query - add tanstack/react-query

--- src/components/catalog/Products/index.tsx
+++ src/components/catalog/Products/index.tsx
@@ -2,8 +2,37 @@ import React from 'react';
 import Product from "../Product";
 import {products} from "../../../common/moks/products";
 import {useStyles} from "./styles";
+import {useQuery} from '@tanstack/react-query'
+import axios from "axios";
+
+interface IData {
+    id: number,
+    slug: string,
+    title: string,
+    imageUrl: string,
+    genderId: number,
+    colorId: number,
+    size: string,
+    categoryId: number,
+    createdAt: string,
+    updatedAt: string,
+    category: {
+        slug: string,
+        title: string
+    }
+}
 
 const Products = () => {
+    const {data} = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const {data} = await axios.get(
+                'http://localhost:5000/products'
+            )
+            return data as IData
+        }
+    })
+
     const classes = useStyles()
     const renderProducts = products.map((element): JSX.Element => {
         return (
